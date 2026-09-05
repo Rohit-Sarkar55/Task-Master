b56131b06f34ad634e06ef7cacb37aba0ff966ec
Rohit-Sarkar55 <sarkarrohit650@gmail.com>
2026-09-05 12:46:40 +0530

    main: temp comment

--- src/main/java/com/airtribe/taskmaster/TaskmasterApplication.java
+++ src/main/java/com/airtribe/taskmaster/TaskmasterApplication.java
@@ -8,6 +8,8 @@ public class TaskmasterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TaskmasterApplication.class, args);
+
+        // temporary comment
     }
 
 }
